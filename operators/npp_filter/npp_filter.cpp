1e806ca7fde4380a4b110970839a2c726f05d9a9
David Su <135102710+DavidSu-Yuan@users.noreply.github.com>
2026-01-09 10:46:56 +0800

    Fix npp_filter on cuda13

--- operators/npp_filter/npp_filter.cpp
+++ operators/npp_filter/npp_filter.cpp
@@ -19,6 +19,7 @@
 
 #include <npp.h>
 #include <gxf/multimedia/video.hpp>
+#include <holoscan/utils/cuda_macros.hpp>
 
 struct NppStreamContext_ : public NppStreamContext {};
 
@@ -52,10 +53,28 @@ static void* adjust_roi_address(uint32_t mask_size, const nvidia::gxf::ColorPlan
 
 void NppFilterOp::initialize() {
   npp_stream_ctx_ = std::make_shared<NppStreamContext_>();
+#if CUDART_VERSION >= 13000
+  // Workaround pending proper NPP support to get stream context in CUDA 13.0+
+  int device = 0;
+  HOLOSCAN_CUDA_CALL_THROW_ERROR(cudaGetDevice(&device), "Failed to get CUDA device");
+
+  cudaDeviceProp prop{};
+  HOLOSCAN_CUDA_CALL_THROW_ERROR(cudaGetDeviceProperties(&prop, device),
+                                 "Failed to get CUDA device properties");
+
+  npp_stream_ctx_->nCudaDeviceId = device;
+  npp_stream_ctx_->nMultiProcessorCount = prop.multiProcessorCount;
+  npp_stream_ctx_->nMaxThreadsPerMultiProcessor = prop.maxThreadsPerMultiProcessor;
+  npp_stream_ctx_->nMaxThreadsPerBlock = prop.maxThreadsPerBlock;
+  npp_stream_ctx_->nSharedMemPerBlock = prop.sharedMemPerBlock;
+  npp_stream_ctx_->nCudaDevAttrComputeCapabilityMajor = prop.major;
+  npp_stream_ctx_->nCudaDevAttrComputeCapabilityMinor = prop.minor;
+#else
   auto nppStatus = nppGetStreamContext(npp_stream_ctx_.get());
   if (NPP_SUCCESS != nppStatus) {
     throw std::runtime_error("Failed to get NPP CUDA stream context");
   }
+#endif
   Operator::initialize();
 }
 
